c9d39f16f7bd44cb1bc140af6ee207a204184621
Akshay Sapra <michaelsapra@Hotmail.com>
2019-12-04 14:44:24 -0500

    Update queries.sql

--- linux_sql/sql/queries.sql
+++ linux_sql/sql/queries.sql
@@ -11,28 +11,23 @@ order by
 	total_mem desc;
 
 #Question 2
-select 
-  host_info.id as host_id, 
-  host_info.hostname as host_name, 
-  rounded_usage.rounded_timestamp as timestamp, 
-  round(
-    avg(
-      host_info.total_mem / 1000 - rounded_usage.memory_free
-    ) / (host_info.total_mem / 1000) * 100, 
-    2
-  ) as avg_used_mem_percentage 
+select
+	host_usage.id,
+	 host_info.hostname, 
+	host_info.mem_total,
+	(
+		avg(
+			(host_info.mem_total - host_usage.memory_free)/host_info.mem_total
+		) * 100
+	)::int as avg_mem_used
 from 
-  (
-    select 
-      *, 
-      date_trunc('hour', timestamp) + date_part('minute', timestamp):: int / 5 * interval '5 min' as rounded_timestamp 
-    from 
-      host_usage
-  ) as rounded_usage 
-  inner join host_info on rounded_usage.host_id = host_info.id 
+	host_usage
+	inner join host_info on host_usage.id = host_info.id
 group by 
-  host_info.id, 
-  rounded_usage.rounded_timestamp 
-order by 
-  host_info.id, 
-  rounded_usage.rounded_timestamp;
+	host_usage.id, 
+	host_info.hostname, 
+	host_info.mem_total,
+	date_trunc('hour', host_usage.curr_time) + interval '5 minute' * round(
+		date_part('minute', host_usage.curr_time) / 5.0
+	)
+order by host_usage.id;
