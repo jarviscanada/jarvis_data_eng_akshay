cb03335f20a58c3a07a330fc1b06efcacdc35c97
Akshay <michaelsapra@hotmail.com>
2020-01-01 08:47:35 +0000

    Test code

---
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/example/TwitterApiTest.java
@@ -0,0 +1,50 @@
+package ca.jrvs.apps.twitter.example;
+
+import com.google.gdata.util.common.base.PercentEscaper;
+import java.util.Arrays;
+import oauth.signpost.OAuthConsumer;
+import oauth.signpost.commonshttp.CommonsHttpOAuthConsumer;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+
+public class TwitterApiTest {
+
+  private static String CONSUMER_KEY = System.getenv("CONSUMER_KEY");
+  private static String CONSUMER_SECRET = System.getenv("CONSUMER_SECRET");
+  private static String ACCESS_TOKEN = System.getenv("ACCESS_TOKEN");
+  private static String TOKEN_SECRET = System.getenv("TOKEN_SECRET");
+
+  public static void main(String[] args) throws Exception {
+
+    // setup oath
+    OAuthConsumer consumer = new CommonsHttpOAuthConsumer(CONSUMER_KEY,
+        CONSUMER_SECRET);
+    consumer.setTokenWithSecret(ACCESS_TOKEN, TOKEN_SECRET);
+    System.out.println(CONSUMER_KEY);
+    System.out.println(CONSUMER_SECRET);
+    System.out.println(ACCESS_TOKEN);
+    System.out.println(TOKEN_SECRET);
+
+    // create an HTTP GET request
+    String status = "today is a good day";
+    PercentEscaper percentEscaper = new PercentEscaper("", false);
+    HttpPost request = new HttpPost(
+        "https://api.twitter.com/1.1/statuses/update.json?status=" + percentEscaper.escape(status));
+
+    // sign the request (add headers)
+    consumer.sign(request);
+
+    System.out.println("HTTP request headers: ");
+    Arrays.stream(request.getAllHeaders()).forEach(System.out::println);
+
+    // send the request
+    HttpClient httpClient = HttpClientBuilder.create().build();
+    HttpResponse httpResponse = httpClient.execute(request);
+    System.out.println(EntityUtils.toString(httpResponse.getEntity()));
+
+  }
+
+}
